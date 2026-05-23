fade9fa907a7f1a2200778b79c310cd8bd217ecf
Réka Sallai <reka.sallai@stud.uni-corvinus.hu>
2026-05-23 10:41:36 +0200

    Added interactive scatterplot

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const ctx = document.getElementById('pieChart');
+// PIE CHART
 
-new Chart(ctx, {
+const pieCtx = document
+    .getElementById('pieChart');
+
+new Chart(pieCtx, {
 
     type: 'pie',
 
@@ -8,22 +11,21 @@ new Chart(ctx, {
 
         labels: [
             'Agree',
-            'Disagree',
-            'Neutral'
+            'Neutral',
+            'Disagree'
         ],
 
         datasets: [{
 
-            data: [52, 28, 20],
+            data: [58, 25, 17],
 
             backgroundColor: [
                 '#66bb6a',
-                '#ef5350',
-                '#bdbdbd'
+                '#bdbdbd',
+                '#ef5350'
             ],
 
             borderWidth: 0
-
         }]
     },
 
@@ -36,9 +38,80 @@ new Chart(ctx, {
             legend: {
                 position: 'bottom'
             }
-
         }
-
     }
+});
 
+
+// SCATTERPLOT
+
+const scatterCtx = document
+    .getElementById('scatterChart');
+
+new Chart(scatterCtx, {
+
+    type: 'scatter',
+
+    data: {
+
+        datasets: [{
+
+            label:
+                'Municipalities',
+
+            data: [
+
+                { x: 120, y: 62 },
+                { x: 180, y: 71 },
+                { x: 220, y: 79 },
+                { x: 310, y: 86 },
+                { x: 95, y: 58 },
+                { x: 270, y: 83 },
+                { x: 150, y: 69 }
+
+            ],
+
+            backgroundColor:
+                '#7e57c2',
+
+            pointRadius: 8
+        }]
+    },
+
+    options: {
+
+        responsive: true,
+
+        plugins: {
+
+            legend: {
+                display: false
+            }
+        },
+
+        scales: {
+
+            x: {
+
+                title: {
+
+                    display: true,
+
+                    text:
+                        'Expenditure (€ per citizen)'
+                }
+            },
+
+            y: {
+
+                title: {
+
+                    display: true,
+
+                    text:
+                        'Citizen Satisfaction (%)'
+                }
+            }
+        }
+    }
 });
